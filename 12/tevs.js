f0b8fb9246ed992b9104dbf5f85643e6c6f2b2f6
slofurno <slofurno@gmail.com>
2015-12-12 01:41:41 -0500

    out of tables

--- 12/tevs.js
+++ 12/tevs.js
@@ -3,17 +3,10 @@
 var fs = require('fs');
 
 fs.readFile("./input.txt", 'utf8', function (err, data) {
-    /*
-    var digits = data.match(/-?[\d]+/g);
-    console.log(digits);
-    var sum = digits.map(x => x|0).reduce((a,c) => a+c);
-    console.log(sum);
-    */
 
     var masked = maskOff(data);
-
     var digits = masked.match(/-?[\d]+/g);
-    console.log(digits);
+
     var sum = digits.map(x => x|0).reduce((a,c) => a+c);
     console.log(sum);
 
@@ -26,9 +19,6 @@ function maskOff (s){
     var stack = [];
     var astack = [];
 
-    var ok = "\x01";
-    var bad = "\x02"; 
-
     for(var i = 0; i < s.length; i++){
         if (s[i] == "{"){
             stack.push(i);
@@ -36,30 +26,17 @@ function maskOff (s){
             var start = stack.pop();
             var sub = s.substring(start, i+1);
             if (sub.indexOf(":\"red\"")>=0){
-                s = mask(s, start, i, bad);        
-            } else{
-            //    s = mask(s, start, i, ok);        
+                s = mask(s, start, i);        
             } 
-
         }
     }
 
     return s;
-    var res = [];
-    for(var i = 0; i < original.length; i++){
-        if (s[i] != bad){
-            res.push(original[i]);
-        }else{
-            res.push("X");
-        }
-    }
-    return res.join("");
 }
 
-function mask (s, start, end, c){
-
+function mask (s, start, end)
+{
     var wtf = [].slice.call(s);
-    var bad = "\x02"; 
 
     for (var i = start; i <= end; i++){
         wtf[i] = "X";
@@ -67,50 +44,3 @@ function mask (s, start, end, c){
 
     return wtf.join("");
 }
-/*
-function bracket (s)
-{
-    var depth = 0;
-    s = "x" + s;
-    var totalSum;
-    var localSum;
-
-    var localBlocked;
-    var tmp = "";
-
-    while(s.length > 1)
-    {
-        s = s.slice(1);
-
-        if (s[i].match(/{/)){
-            depth++;
-            continue;
-        }
-
-        if(s[i].match(/}/){
-            depth--;
-            continue;
-        }
-        
-        tmp = s[i].match(/-?[\d]/); 
-        tmp = tmp ? tmp[0] : tmp;
-
-        if(tmp){
-            var s =
-            var amt = 
-        }
-    }
-
-}
-
-function cycle (o){
-    var it = o;
-
-    if (typeof(o) === "object"){
-        var keys = Object.keys(o);
-        var red = keys.filter(x => x === "red");  
-         
-    }
-}
-*/
-
